da97594e0a68be5d564f9f0bde31254bcd16240b
Carlos Edilson Junior <carlosedilsonabj@gmail.com>
2024-07-09 01:36:50 -0300

    feat: create two sections of initial page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,24 @@
 import "@/styles/global.css"
-
 import { Slot } from "expo-router"
 import { StatusBar, View } from "react-native"
+import { useFonts, Inter_500Medium, Inter_400Regular, Inter_600SemiBold } from "@expo-google-fonts/inter"
+
+import { Loading } from "@/components/Loading"
 
 export default function Layout() {
+  const [fonstLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold
+  })
+
+  if (!fonstLoaded) {
+    return <Loading />
+  }
+
   return (
     <View className="flex-1 bg-zinc-950">
-      <StatusBar barStyle={"light-content"} backgroundColor={'transparent'} translucent />
+      <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent />
 
       <Slot />
     </View>
